9b9e002a8f585d5aa14857785603ed435b8fab58
Naman Bhatt <106633446+Namanux@users.noreply.github.com>
2023-08-13 19:32:05 +1200

    ShadCN Button Comp
    ShadCN Button Component added to Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main>
+      <Button>Click me</Button>
     </main>
   )
 }
