d888278f6392dbc20614c30170c382d12b98554c
Naman Bhatt <106633446+Namanux@users.noreply.github.com>
2023-08-20 16:40:11 +1200

    NextConfig updated
    output: 'export', copied pest in nect.config.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/home/Hero";
+
 export default function Home() {
   return (
     <main className="mb-auto">
       
-      
+      <Hero />
     
     </main>
     
---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+        <div className="flex justify-center items-center">
+            <h2 className="mt-20 text-6xl text-center font-bold">UX / Ui Design <br/>& <br/>Custom web App Development</h2>
+            
+            </div>
+    );
+}
+
+export default Hero;
